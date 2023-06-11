898ae122eaba25a00eed2e45a7896b6e9fc0c2c8
aytbunbun <aytbunbun@users.noreply.github.com>
2023-06-11 12:14:54 +0000

    deploy: 2414f12fb8f274e07a1da4b26e3fa1bbccf2089e

--- _next/static/RftNWZ5cXUCGhmgYyV9lc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/92fb137f32c15047.css","static/chunks/pages/index-690c1c1bd41b58b0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/nfIpSFDEeeVAsTfKTkak3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/92fb137f32c15047.css","static/chunks/pages/index-a667045d4fe8849c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
