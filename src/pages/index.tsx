52bf42f36425b9116dec6ebc2c8fc4bd9bd2570f
aytbunbun <135977596+aytbunbun@users.noreply.github.com>
2023-06-14 15:29:28 +0900

    空白にのみ置ける

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,31 +24,33 @@ const Home = () => {
     { y: 1, x: -1 },
   ];
   const onClick = (x: number, y: number) => {
-    console.log(x, y);
-    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    for (const dir of directions) {
-      for (let i = 1; i < 8; i++)
-        if (board[y + dir.y * i] === undefined) {
-          break;
-        } else if (board[y + dir.y * i][x + dir.x * i] === turnColor) {
-          break;
-        } else if (board[y + dir.y * i][x + dir.x * i] === 0) {
-          break;
-        } else if (
-          board[y + dir.y * i][x + dir.x * i] !== turnColor &&
-          board[y + dir.y * (i + 1)] !== undefined &&
-          board[y + dir.y * (i + 1)][x + dir.x * (i + 1)] === turnColor
-        ) {
-          newBoard[y][x] = turnColor;
-          for (let m = 1; m < i + 1; m++) {
-            newBoard[y + dir.y * m][x + dir.x * m] = turnColor;
-          }
+    if (board[y][x] === 0) {
+      console.log(x, y);
+      const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+      for (const dir of directions) {
+        for (let i = 1; i < 8; i++)
+          if (board[y + dir.y * i] === undefined) {
+            break;
+          } else if (board[y + dir.y * i][x + dir.x * i] === turnColor) {
+            break;
+          } else if (board[y + dir.y * i][x + dir.x * i] === 0) {
+            break;
+          } else if (
+            board[y + dir.y * i][x + dir.x * i] !== turnColor &&
+            board[y + dir.y * (i + 1)] !== undefined &&
+            board[y + dir.y * (i + 1)][x + dir.x * (i + 1)] === turnColor
+          ) {
+            newBoard[y][x] = turnColor;
+            for (let m = 1; m <= i + 1; m++) {
+              newBoard[y + dir.y * m][x + dir.x * m] = turnColor;
+            }
 
-          setTurnColor(3 - turnColor);
-          break;
-        }
+            setTurnColor(3 - turnColor);
+            break;
+          }
+      }
+      setBoard(newBoard);
     }
-    setBoard(newBoard);
   };
   return (
     <div className={styles.container}>
