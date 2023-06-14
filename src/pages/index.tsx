31adf0cf888fd3daeeb685319775949796aec194
aytbunbun <135977596+aytbunbun@users.noreply.github.com>
2023-06-14 19:46:53 +0900

    パス以外

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,14 @@ const Home = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const colors: string[] = ['', '黒のターン', '白のターン'];
   const directions: { y: number; x: number }[] = [
     { y: -1, x: 0 },
     { y: -1, x: 1 },
@@ -23,37 +24,91 @@ const Home = () => {
     { y: 0, x: -1 },
     { y: 1, x: -1 },
   ];
-  const onClick = (x: number, y: number) => {
-    if (board[y][x] === 0) {
-      console.log(x, y);
-      const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-      for (const dir of directions) {
-        for (let i = 1; i < 8; i++)
-          if (board[y + dir.y * i] === undefined) {
-            break;
-          } else if (board[y + dir.y * i][x + dir.x * i] === turnColor) {
-            break;
-          } else if (board[y + dir.y * i][x + dir.x * i] === 0) {
-            break;
-          } else if (
-            board[y + dir.y * i][x + dir.x * i] !== turnColor &&
-            board[y + dir.y * (i + 1)] !== undefined &&
-            board[y + dir.y * (i + 1)][x + dir.x * (i + 1)] === turnColor
-          ) {
-            newBoard[y][x] = turnColor;
+  const gameSystem = (
+    x: number,
+    y: number,
+    isOrange: boolean,
+    turn: number,
+    newBoard: number[][]
+  ) => {
+    let isOrangePlace = false;
+    for (const dir of directions) {
+      for (let i = 1; i < 8; i++) {
+        if (newBoard[y + dir.y * i] === undefined) {
+          break;
+        } else if (newBoard[y + dir.y * i][x + dir.x * i] === turn) {
+          break;
+        } else if (newBoard[y + dir.y * i][x + dir.x * i] === 0) {
+          break;
+        } else if (newBoard[y + dir.y * i][x + dir.x * i] === 3) {
+          break;
+        } else if (
+          newBoard[y + dir.y * i][x + dir.x * i] === 3 - turn &&
+          newBoard[y + dir.y * (i + 1)] !== undefined &&
+          newBoard[y + dir.y * (i + 1)][x + dir.x * (i + 1)] === turn
+        ) {
+          if (!isOrange) {
+            newBoard[y][x] = turn;
             for (let m = 1; m <= i + 1; m++) {
-              newBoard[y + dir.y * m][x + dir.x * m] = turnColor;
+              newBoard[y + dir.y * m][x + dir.x * m] = turn;
             }
-
-            setTurnColor(3 - turnColor);
-            break;
+          } else {
+            if (i > 0) {
+              isOrangePlace = true;
+            }
           }
+          break;
+        }
       }
-      setBoard(newBoard);
+    }
+    return { board: newBoard, color: isOrangePlace };
+  };
+  const onClick = (x: number, y: number) => {
+    console.table(board);
+    console.log(x, y);
+    if (board[y][x] === 3) {
+      const stones = gameSystem(x, y, false, turnColor, board).board;
+
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          stones[i][j] %= 3;
+        }
+      }
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          if (gameSystem(j, i, true, 3 - turnColor, stones).color && stones[i][j] === 0) {
+            stones[i][j] = 3;
+          }
+        }
+      }
+
+      let nextTurnColor = 3 - turnColor;
+      if (!stones.some((row) => row.includes(3))) {
+        nextTurnColor = turnColor;
+      }
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          stones[i][j] %= 3;
+        }
+      }
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          if (gameSystem(j, i, true, nextTurnColor, stones).color && stones[i][j] === 0) {
+            stones[i][j] = 3;
+          }
+        }
+      }
+      setTurnColor(nextTurnColor);
+      setBoard(stones);
     }
   };
   return (
     <div className={styles.container}>
+      <div
+        style={{ color: turnColor === 1 ? '#000' : '#fff', backgroundColor: 'green', fontSize: 60 }}
+      >
+        {colors[turnColor]}
+      </div>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
@@ -61,7 +116,7 @@ const Home = () => {
               {color !== 0 && (
                 <div
                   className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                  style={{ background: color === 1 ? '#000' : color === 2 ? '#fff' : '#fa0' }}
                 />
               )}
             </div>
